39338a393fb3105e702f6c80f3b8f03ff2e5adc1
Hüseyin Bera Bulut <45367283+berabulut@users.noreply.github.com>
2019-06-28 21:44:35 +0300

    Add files via upload

---
+++ Cryptography Tool/CTool.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptography_Tool
+{
+    public partial class CTool : Form
+    {
+        public CTool()
+        {
+            InitializeComponent();
+        }
+
+        private void encrypt_Button_Click(object sender, EventArgs e)
+        {
+            if (decrypted_Text.Text.Length > 0 && input_Alphabet.Text.Length > 0 && inputKey.Text.Length > 0)
+            {
+                vigenere_Cipher vigenere = new vigenere_Cipher();
+                vigenere.alphabet = input_Alphabet.Text.ToUpper();
+                vigenere.plainText = decrypted_Text.Text.ToUpper();
+                vigenere.key = inputKey.Text.ToUpper();
+
+                vigenere.SetKeyStream(vigenere.keyStream, vigenere.plainText, vigenere.key);
+                vigenere.EnumerationAlphabet(vigenere.Alphabet, vigenere.alphabet);
+                vigenere.CheckTextPairsWithAlphabet(vigenere.alphabet, vigenere.key, vigenere.pair);
+                if (vigenere.pair == false)
+                {
+                    MessageBox.Show("key and alphabet doesn't pair");
+                    return;
+                }
+                vigenere.CheckTextPairsWithAlphabet(vigenere.alphabet, vigenere.plainText, vigenere.pair);
+                if (vigenere.pair == false)
+                {
+                    MessageBox.Show("text and alphabet doesn't pair");
+                    return;
+                }
+                vigenere.EnumerationKeyStream(vigenere.KeyStream, vigenere.Alphabet, vigenere.keyStream, vigenere.plainText, vigenere.alphabet);
+                vigenere.EnumerationCipherText(vigenere.cipherNumber, vigenere.Alphabet, vigenere.KeyStream, vigenere.alphabet, vigenere.plainText);
+                vigenere.Encrypt(vigenere.CipherText, vigenere.cipherNumber, vigenere.Alphabet, vigenere.alphabet, vigenere.plainText);
+
+                for (int i = 0; i < vigenere.plainText.Length; i++)
+                {
+                    encrypted_Text.Text += vigenere.CipherText[i].ToString();
+
+                    if (encrypted_Text.Text.Length > vigenere.plainText.Length)
+                    {
+                        encrypted_Text.Text = "";
+                        for (int j = 0; j < vigenere.plainText.Length; j++)
+                            encrypted_Text.Text += vigenere.CipherText[j].ToString();
+                    }
+                }
+            }
+
+        }
+
+        private void decrypt_Button_Click(object sender, EventArgs e)
+        {
+            if (encrypted_Text.Text.Length > 0 && input_Alphabet.Text.Length > 0 && inputKey.Text.Length > 0)
+            {
+                vigenere_Cipher vigenere = new vigenere_Cipher();
+                vigenere.alphabet = input_Alphabet.Text.ToUpper();
+                vigenere.cipherText = encrypted_Text.Text.ToUpper();
+                vigenere.key = inputKey.Text.ToUpper();
+
+                vigenere.SetKeyStream(vigenere.keyStream, vigenere.cipherText, vigenere.key);
+                vigenere.EnumerationAlphabet(vigenere.Alphabet, vigenere.alphabet);
+                vigenere.CheckTextPairsWithAlphabet(vigenere.alphabet, vigenere.key, vigenere.pair);
+                if (vigenere.pair == false)
+                {
+                    MessageBox.Show("key and alphabet doesn't pair");
+                    return;
+                }
+                vigenere.CheckTextPairsWithAlphabet(vigenere.alphabet, vigenere.cipherText, vigenere.pair);
+                if (vigenere.pair == false)
+                {
+                    MessageBox.Show("text and alphabet doesn't pair");
+                    return;
+                }
+
+
+                vigenere.EnumerationKeyStream(vigenere.KeyStream, vigenere.Alphabet, vigenere.keyStream, vigenere.cipherText, vigenere.alphabet);
+                vigenere.EnumerationCipherText(vigenere.cipherNumber, vigenere.Alphabet, vigenere.KeyStream, vigenere.alphabet, vigenere.cipherText);
+                vigenere.EnumerationPlainText(vigenere.plainNumber, vigenere.cipherNumber, vigenere.Alphabet, vigenere.KeyStream, vigenere.alphabet, vigenere.cipherText);
+                vigenere.Decyrpt(vigenere.PlainText, vigenere.cipherNumber, vigenere.Alphabet, vigenere.alphabet, vigenere.cipherText);
+
+                for (int i = 0; i < vigenere.cipherText.Length; i++)
+                {
+                    decrypted_Text.Text += vigenere.PlainText[i].ToString();
+
+                    if (decrypted_Text.Text.Length > vigenere.cipherText.Length)
+                    {
+                        decrypted_Text.Text = "";
+                        for (int j = 0; j < vigenere.cipherText.Length; j++)
+                            decrypted_Text.Text += vigenere.PlainText[j].ToString();
+                    }
+                }
+            }
+        }
+       
+        private void Input_Alphabet_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                warning.Visible = true;
+            }
+
+            else
+                warning.Visible = false;
+        }
+
+        private void InputKey_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                warning.Visible = true;
+            }
+            else
+                warning.Visible = false;
+        }
+
+        private void Decrypted_Text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                warning.Visible = true;
+            }
+
+            else
+                warning.Visible = false;
+        }
+
+        private void Encrypted_Text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                warning.Visible = true;
+            }
+
+            else
+                warning.Visible = false;
+        }
+
+    }
+}
---
+++ Cryptography Tool/vigenere_Cipher.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_Tool
+{
+    class vigenere_Cipher
+    {
+        public string plainText { get; set; }
+        public string cipherText { get; set; }
+        public string key { get; set; }
+        public string alphabet { get; set; }
+
+        public string[] CipherText;
+        public string[] PlainText;
+        public string[] keyStream;
+        public int[] cipherNumber;
+        public int[] plainNumber;
+        public bool pair;
+        
+
+        public List<Tuple<string, int>> Alphabet = new List<Tuple<string, int>>();
+        public List<Tuple<string, int>> KeyStream = new List<Tuple<string, int>>();
+       
+
+       
+
+        public void CheckTextPairsWithAlphabet(string alphabet, string text, bool pair)
+        {
+            string[] check = new string[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for(int j = 0; j < this.alphabet.Length; j++)
+                    if(text[i] == this.alphabet[j])
+                    {
+                        check[i] = this.alphabet[j].ToString();
+                    }
+            }
+
+            if (check[1]!= null && check.Length == text.Length)
+                this.pair = true;
+            else
+                this.pair = false;            
+        }
+
+        public void SetKeyStream(string [] keyStream, string pplainText, string key)
+        {
+            var x = pplainText.Length;           
+            this.keyStream = new string[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                this.keyStream[i] = this.key[i % key.Length].ToString();
+            }
+            string.Join("", this.keyStream);
+        }
+
+
+        public void EnumerationAlphabet(List<Tuple<string, int>> Alphabet, string alphabet)
+        {
+
+            for (int i = 0; i < this.alphabet.Length; i++)
+            {
+                this.Alphabet.Add(new Tuple<string, int>(this.alphabet[i].ToString(), i));
+            }
+        }
+
+        public void EnumerationKeyStream(List<Tuple<string, int>> KeyStream, List<Tuple<string, int>> Alphabet, string[] keyStream, string text, string alphabet)
+        {
+            for (int i = 0; i < text.Length; i++)    //Giving numbers to KeyStream according to Alphabet
+            {
+                for (int j = 0; j < this.alphabet.Length; j++)
+                {
+                    if (this.Alphabet[j].Item1 == keyStream[i % key.Length])
+                        this.KeyStream.Add(new Tuple<string, int>(this.keyStream[i], this.Alphabet[j].Item2));
+                }
+            }
+        }
+
+        
+        public void EnumerationCipherText(int[] cipherNumber, List<Tuple<string, int>> Alphabet, List<Tuple<string, int>> KeyStream, string alphabet, string text)
+        {
+            this.cipherNumber = new int[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < this.alphabet.Length; j++)
+                {
+                    if (text[i].ToString() == this.Alphabet[j].Item1)
+                    {
+                        this.cipherNumber[i] = (this.Alphabet[j].Item2 + this.KeyStream[i].Item2) % this.alphabet.Length;
+                    }
+                }
+            }
+        }
+
+        public void Encrypt(string[] CipherText, int[] cipherNumber, List<Tuple<string, int>> Alphabet, string alphabet, string plainText)
+        {
+            this.CipherText = new string[this.plainText.Length];
+
+            for (int i = 0; i < this.cipherNumber.Length; i++)
+            {
+                for (int j = 0; j < this.alphabet.Length; j++)
+                {
+                    if (this.cipherNumber[i] == this.Alphabet[j].Item2)
+                    {
+                        this.CipherText[i] = this.Alphabet[j].Item1;
+                    }
+                }
+            }
+        }
+
+        public void EnumerationPlainText(int[] plainNumber, int[] cipherNumber, List<Tuple<string, int>> Alphabet, List<Tuple<string, int>> KeyStream, string alphabet, string cipherText)
+        {
+            this.plainNumber = new int[this.cipherText.Length];
+
+            for (int i = 0; i < this.cipherText.Length; i++)
+            {
+                for (int j = 0; j < this.alphabet.Length; j++)
+                {
+                    if (this.cipherText[i].ToString() == this.Alphabet[j].Item1)
+                    {
+                        this.cipherNumber[i] = this.Alphabet[j].Item2;
+                    }
+                }
+            }
+
+            for (int i = 0; i < this.cipherText.Length; i++)
+            {
+                this.plainNumber[i] = this.cipherNumber[i] - this.KeyStream[i].Item2;
+                if (this.plainNumber[i] < 0)
+                    this.plainNumber[i] += this.cipherText.Length;
+            }
+        }
+
+
+        public void Decyrpt(string[] PlainText, int[] plainNumber, List<Tuple<string, int>> Alphabet, string alphabet, string cipherText)
+        {
+            this.PlainText = new string[this.cipherText.Length];
+
+            for (int i = 0; i < this.cipherText.Length; i++)
+            {
+                for (int j = 0; j < this.alphabet.Length; j++)
+                {
+                    if (this.plainNumber[i] == this.Alphabet[j].Item2)
+                    {
+                        this.PlainText[i] = this.Alphabet[j].Item1;
+                    }
+                }
+            }
+        }
+    }
+}
